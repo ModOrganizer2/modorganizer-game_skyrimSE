6b0c9d64e80868078715399da9097a4b0093f225
Silarn <jrim@rimpo.org>
2018-05-04 00:15:47 -0500

    Update translation file

--- src/game_skyrimse_en.ts
+++ src/game_skyrimse_en.ts
@@ -61,12 +61,12 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="281"/>
+        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="286"/>
         <source>failed to query registry path (preflight): %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="288"/>
+        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="293"/>
         <source>failed to query registry path (read): %1</source>
         <translation type="unfinished"></translation>
     </message>
