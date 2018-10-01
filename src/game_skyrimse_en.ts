f1ef62a4ae6799a2da3d7e10368cce5cfab0ef9c
Silarn <jrim@rimpo.org>
2018-10-01 00:10:23 -0500

    Add 'creation' game lib for creation engine changes, eliminate duplicate plugin code

--- src/game_skyrimse_en.ts
+++ src/game_skyrimse_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>GameSkyrimSE</name>
     <message>
-        <location filename="gameskyrimse.cpp" line="117"/>
+        <location filename="gameskyrimse.cpp" line="116"/>
         <source>Adds support for the game Skyrim Special Edition.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,48 +12,48 @@
 <context>
     <name>GamebryoSaveGameInfoWidget</name>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.ui" line="39"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.ui" line="39"/>
         <source>Save #</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.ui" line="51"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.ui" line="51"/>
         <source>Character</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.ui" line="63"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.ui" line="63"/>
         <source>Level</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.ui" line="75"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.ui" line="75"/>
         <source>Location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.ui" line="87"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.ui" line="87"/>
         <source>Date</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.cpp" line="73"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.cpp" line="73"/>
         <source>Has Script Extender Data</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.cpp" line="78"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.cpp" line="78"/>
         <source>Missing ESPs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.cpp" line="111"/>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.cpp" line="149"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.cpp" line="111"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.cpp" line="149"/>
         <source>None</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegameinfowidget.cpp" line="117"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegameinfowidget.cpp" line="117"/>
         <source>Missing ESLs</source>
         <translation type="unfinished"></translation>
     </message>
@@ -61,43 +61,43 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="286"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamegamebryo.cpp" line="286"/>
         <source>failed to query registry path (preflight): %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamegamebryo.cpp" line="293"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamegamebryo.cpp" line="293"/>
         <source>failed to query registry path (read): %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryogameplugins.cpp" line="108"/>
-        <location filename="skyrimsegameplugins.cpp" line="80"/>
+        <location filename="../../game_gamebryo/src/creation/creationgameplugins.cpp" line="101"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryogameplugins.cpp" line="128"/>
         <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryobsainvalidation.cpp" line="57"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryobsainvalidation.cpp" line="57"/>
         <source>failed to deactivate BSA invalidation in &quot;%1&quot; (errorcode %2)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryobsainvalidation.cpp" line="90"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryobsainvalidation.cpp" line="90"/>
         <source>failed to activate BSA invalidation in &quot;%1&quot; (errorcode %2)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryodataarchives.cpp" line="34"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryodataarchives.cpp" line="35"/>
         <source>failed to set archive key (errorcode %1)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegame.cpp" line="92"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegame.cpp" line="92"/>
         <source>failed to open %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../game_gamebryo/src/gamebryosavegame.cpp" line="102"/>
+        <location filename="../../game_gamebryo/src/gamebryo/gamebryosavegame.cpp" line="102"/>
         <source>wrong file format - expected %1 got %2</source>
         <translation type="unfinished"></translation>
     </message>
