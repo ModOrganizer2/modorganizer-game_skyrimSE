d86fa231dd846f079ed8345a4ce12554ecf6b589
Mikaël Capelle <capelle.mikael@gmail.com>
2021-02-02 20:39:13 +0100

    Update translation file.

--- src/game_skyrimse_en.ts
+++ src/game_skyrimse_en.ts
@@ -4,7 +4,12 @@
 <context>
     <name>GameSkyrimSE</name>
     <message>
-        <location filename="gameskyrimse.cpp" line="126"/>
+        <location filename="gameskyrimse.cpp" line="119"/>
+        <source>Skyrim Special Edition Support Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gameskyrimse.cpp" line="129"/>
         <source>Adds support for the game Skyrim Special Edition.</source>
         <translation type="unfinished"></translation>
     </message>
