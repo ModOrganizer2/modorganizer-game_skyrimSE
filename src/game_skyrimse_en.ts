6a48522f80c707892a0d4f3dd4283bf71cc2565b
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-14 18:05:12 +0200

    Update following gamebryo updates for myGamesPath() in game feature.

--- src/game_skyrimse_en.ts
+++ src/game_skyrimse_en.ts
@@ -4,12 +4,12 @@
 <context>
     <name>GameSkyrimSE</name>
     <message>
-        <location filename="gameskyrimse.cpp" line="186"/>
+        <location filename="gameskyrimse.cpp" line="180"/>
         <source>Skyrim Special Edition Support Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gameskyrimse.cpp" line="196"/>
+        <location filename="gameskyrimse.cpp" line="190"/>
         <source>Adds support for the game Skyrim Special Edition.</source>
         <translation type="unfinished"></translation>
     </message>
